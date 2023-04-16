587e2c5c264d539e37d9b2de36331b7661b2585d
snowflakethrowaway69 <snowflakethrowaway69@gmail.com>
2023-04-15 21:11:54 -0700

    Load all rows

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,7 +6,7 @@ import streamlit
 my_cnx = snowflake.connector.connect(**streamlit.secrets['snowflake'])
 my_cur = my_cnx.cursor()
 my_cur.execute('SELECT * FROM FRUIT_LOAD_LIST')
-my_data_row = my_cur.fetchone()
+my_data_rows = my_cur.fetchall()
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -41,5 +41,5 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 fruity_df = streamlit.dataframe(fruityvice_normalized)
 
 
-streamlit.text('The fruit load list contains:')
-streamlit.text(my_data_row)
+streamlit.header('The fruit load list contains:')
+streamlit.dataframe(my_data_rows)
