486921ae65bba600776595378602b31b2e4dd27a
snowflakethrowaway69 <snowflakethrowaway69@gmail.com>
2023-04-15 21:07:38 -0700

    Add snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,12 @@
 import pandas
 import requests
+import snowflake.connector
 import streamlit
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
 
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
@@ -33,3 +39,7 @@ fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/{fruit_cho
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # Create streamlit dataframe from pandas df
 fruity_df = streamlit.dataframe(fruityvice_normalized)
+
+
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
