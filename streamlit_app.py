4ed0cf991a0cdd77906ab8a1498bead0d6abac73
snowflakethrowaway69 <snowflakethrowaway69@gmail.com>
2023-04-15 17:52:23 -0700

    Get user fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,6 @@ import streamlit
 my_fruit_list = pandas.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
 streamlit.title('My Parents New Healthy Diner')
 streamlit.header('Breakfast Favorites')
@@ -23,7 +22,12 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 # Display the table on the page.
 streamlit.dataframe(fruits_to_show)
 
-streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header('Fruityvice Fruit Advice!')
+
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get(f'https://fruityvice.com/api/fruit/{fruit_choice}')
 
 # Create pandas df from normalized json attributes
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
